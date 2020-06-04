201cca94141e10f0bb2bf00150e243dd5f3b88b2
Owais Khan <owaiskhandawar310@gmail.com>
2020-06-04 06:54:17 +0500

    updated src/index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 function Greet(){
-  return <h1>hwllo world</h1> ;
+  return <h1>Hello World! welcome in React.</h1> ;
 }
 ReactDOM.render(<Greet/>,document.querySelector('#root'));
