348688dbe212d467ebb759d8521d602d19a94c5b
Owais Khan <owaiskhandawar310@gmail.com>
2020-06-07 22:38:24 +0500

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 function Greet(){
-  return <h1>Hello World! welcome to React.</h1> ;
+  return React.createElement('h1',{},"hello world")
 }
-ReactDOM.render(<Greet/>,document.querySelector('#root'));
+const el = <img/>
+ReactDOM.render(el,document.querySelector('#root'));
