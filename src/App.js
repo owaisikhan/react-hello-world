2fd348bfd6d381d5cc676371b0c3be5ea634f3e2
Owais Khan <owaiskhandawar310@gmail.com>
2020-06-08 16:09:51 +0500

    Add new component Courses to Parent Componet App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Student from './Student'
-
+import Courses from './Courses'
 function App(){
     return (
         <React.Fragment>
@@ -8,6 +8,11 @@ function App(){
             <Student Name="Owais Khan" Org="UET Peshawar" Deptt="Electrical" Year="Final Year"  City="Peshawar"/>
             <Student Name="Yasin" Org="UET Peshawar" Deptt="Electrical" Year="Final Year" City="Jalozai"/>
             <Student Name="Usman" Org="UET Peshawar" Deptt="Electrical" Year="Final Year" City="Kamra"/>
+            <hr/>
+            <h1>Courses</h1>
+            <Courses title="Control Systems" chr={3}/>
+            <Courses title="Wireless Communication" chr={3}/>
+            <Courses title="Digital Communication" chr={3}/>
         </React.Fragment>
     )
 }
---
+++ src/Courses.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+ const Courses = ({title,chr})=>
+         <p> course title: {title}, credit hours: {chr}</p>
+
+export default Courses;
